4ed7dccb7e6ccbc6837ea7e16ecb20975e00cb12
zmxnina. <mz2958@nyu.edu>
2024-02-13 01:16:57 -0500

    Finished raw data munging workshop!

--- analyze.py
+++ analyze.py
@@ -1,1 +1,43 @@
 # Place code below to do the analysis part of the assignment.
+import os
+import csv
+
+def main():
+    filepath = os.path.join("data", "clean_data.csv")
+    file = open(filepath, "r")
+    csv_reader = csv.DictReader(file)
+
+    # Dictionary to store all monthly anomalies for each decade
+    anomalies = {}
+    months = list(next(csv_reader).keys())[1:13]
+    last_year = 0
+
+    for row in csv_reader:
+        year = int(row["Year"])
+        last_year = max(last_year, year)
+
+        for m in months:
+            temp = float(row[m])
+            decade = (year // 10) * 10
+            # Add temp to the corresponding decade
+            if decade in anomalies:
+                anomalies[decade].append(temp)
+            else:
+                anomalies[decade] = [temp]
+    
+    # Dictionary to store the average anomaly for each decade
+    average_anomalies = {}
+    for decade, anomaly_list in anomalies.items():
+        average_anomaly = sum(anomaly_list) / len(anomaly_list)
+        average_anomalies[decade] = average_anomaly
+    
+    print("The average temperature anomaly in degrees Farenheit for each decade since 1880 are as follows,")
+    for decade, average_anomaly in average_anomalies.items():
+        # For the latest decade:
+        if decade == max(average_anomalies):
+            print(f"{decade} to {last_year}: {format(average_anomaly, '.1f')}")
+        else:
+            print(f"{decade} to {decade + 9}: {format(average_anomaly, '.1f')}")
+
+if __name__ == "__main__":
+    main()
--- munge.py
+++ munge.py
@@ -1,1 +1,65 @@
 # Place code below to do the munging part of this assignment.
+import os
+
+"""
+Accepts a temperature anomaly in 0.01 degrees Celsius as a string.
+Converts anomaly to Farenheit.
+Returns the result formatted to one decimal place as a string. 
+"""
+def convert_temperature(temp):
+    return format(float(temp) / 100 * (9/5), ".1f")
+
+"""
+Accepts the temperature data from a specific year as a list.
+Checks the list for missing values ("***").
+Replaces any missing values with the annual average anomaly.
+Returns the handled list.
+"""
+def handle_missing_values(temp_data):
+    for i in range(1, len(temp_data)):
+        if temp_data[i][0] == "*":
+            total = 0
+            count = 0
+            for j in range(1, 13):
+                if temp_data[j][0] != "*":
+                    total += float(temp_data[j])
+                    count += 1
+            if count > 0:
+                mean = round(total / count)
+                temp_data[i] = str(mean)
+            else:
+                # Set to zero if all monthly values are missing
+                temp_data[i] = "0" 
+    return temp_data
+
+def main():
+    input_filepath = os.path.join("data", "GLB.Ts+dSST.txt")
+    output_filepath = os.path.join("data", "clean_data.csv")
+
+    input_file = open(input_filepath, "r", encoding="utf_8")
+    output_file = open(output_filepath, 'w', encoding="utf_8")
+
+    all_lines = input_file.readlines()
+    first_heading_found = False
+
+    for line in all_lines:
+        if line[:4] == "Year":
+            # Only retain the first line of column headings
+            if not first_heading_found:
+                heading = line.split()[:-1]
+                # Standardize comma "," as the value separator
+                row = ",".join(heading)
+                output_file.write(row + "\n") 
+                first_heading_found = True
+        elif line[:4].isdigit():
+            temp_data = line.split()[:-1]
+            temp_data = handle_missing_values(temp_data)
+            temp_data[1:] = map(convert_temperature, temp_data[1:])
+            row = ",".join(temp_data)
+            output_file.write(row + "\n")
+
+    input_file.close()
+    output_file.close()
+
+if __name__ == "__main__":
+    main()
